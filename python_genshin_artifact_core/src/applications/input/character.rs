713083f4e8660180bb8e6d84cfd53c9f18180a41
洛水居室 <luoshuijs@outlook.com>
2023-10-28 22:07:26 +0800

    :bug: Add getter/setter

--- python_genshin_artifact_core/src/applications/input/character.rs
+++ python_genshin_artifact_core/src/applications/input/character.rs
@@ -45,73 +45,89 @@ impl PyCharacterInterface {
         })
     }
 
+    #[getter]
     pub fn get_name(&self) -> PyResult<String> {
         Ok(self.name.clone())
     }
 
+    #[getter]
     pub fn get_level(&self) -> PyResult<usize> {
         Ok(self.level)
     }
 
+    #[getter]
     pub fn get_ascend(&self) -> PyResult<bool> {
         Ok(self.ascend)
     }
 
+    #[getter]
     pub fn get_constellation(&self) -> PyResult<i32> {
         Ok(self.constellation)
     }
 
+    #[getter]
     pub fn get_skill1(&self) -> PyResult<usize> {
         Ok(self.skill1)
     }
 
+    #[getter]
     pub fn get_skill2(&self) -> PyResult<usize> {
         Ok(self.skill2)
     }
 
+    #[getter]
     pub fn get_skill3(&self) -> PyResult<usize> {
         Ok(self.skill3)
     }
 
+    #[getter]
     pub fn get_params(&self) -> PyResult<Option<Py<PyDict>>> {
         Ok(self.params.clone())
     }
 
+    #[setter]
     pub fn set_name(&mut self, name: String) -> PyResult<()> {
         self.name = name;
         Ok(())
     }
 
+    #[setter]
     pub fn set_level(&mut self, level: usize) -> PyResult<()> {
         self.level = level;
         Ok(())
     }
 
+    #[setter]
     pub fn set_ascend(&mut self, ascend: bool) -> PyResult<()> {
         self.ascend = ascend;
         Ok(())
     }
 
+    #[setter]
     pub fn set_constellation(&mut self, constellation: i32) -> PyResult<()> {
         self.constellation = constellation;
         Ok(())
     }
 
+    #[setter]
     pub fn set_skill1(&mut self, skill1: usize) -> PyResult<()> {
         self.skill1 = skill1;
         Ok(())
     }
 
+    #[setter]
     pub fn set_skill2(&mut self, skill2: usize) -> PyResult<()> {
         self.skill2 = skill2;
         Ok(())
     }
 
+    #[setter]
     pub fn set_skill3(&mut self, skill3: usize) -> PyResult<()> {
         self.skill3 = skill3;
         Ok(())
     }
 
+    #[setter]
     pub fn set_params(&mut self, params: Option<Py<PyDict>>) -> PyResult<()> {
         self.params = params;
         Ok(())
